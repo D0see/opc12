ddbfdf97c7e32318cb1bbbde6421bc0865e1ea1a
D0see <317Y17@gmail.com>
2026-08-28 09:49:37 +0200

    save

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Dto\User\Input\UserCreationInputDTO;
-use App\Entity\User;
 use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('api/user')]
 final class UserController extends AbstractController
@@ -17,21 +19,28 @@ final class UserController extends AbstractController
 
     public function __construct(
         private readonly SerializerInterface $serializer,
-        private readonly UserService $userService
+        private readonly ValidatorInterface $validator,
+        private readonly UserService $userService,
     ){}
-    #[Route(name: 'user_create', methods: ['POST'])]
+    #[Route(path: '/create', name: 'user_create', methods: ['POST'])]
     public function create(
         Request $request
     ): JsonResponse
     {
         $userCreationInputDTO = $this->serializer->deserialize($request->getContent(), UserCreationInputDTO::class, 'json');
 
+        $errors = $this->validator->validate($userCreationInputDTO);
+
+        if (count($errors) > 0) {
+            throw new HttpException(statusCode: Response::HTTP_BAD_REQUEST, message: json_encode($errors));
+        }
+
         $this->userService->createUser(
-            userName: $userCreationInputDTO->getUsername(),
+            username: $userCreationInputDTO->getUsername(),
             password: $userCreationInputDTO->getPassword(),
             code: $userCreationInputDTO->getPostalCode()
         );
         
-        return new JsonResponse();
+        return new JsonResponse(status: Response::HTTP_CREATED);
     }
 }
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -2,27 +2,60 @@
 
 namespace App\Service;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
 class UserService {
     public function __construct(
+        private readonly UserPasswordHasherInterface $userPasswordHasher,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ValidatorInterface $validator,
         private readonly PostalCodeService $postalCodeService,
+        private readonly UserRepository $userRepository,
     ){}
 
     /**
-     * @param string $userName
+     * @param string $username
      * @param string $password clear password
      * @param string $code the postalCode code
      * @return void
      */
     public function createUser(
-        string $userName,
+        string $username,
         string $password,
         string $code
-    ) {
-        $this->postalCodeService->findOrCreatePostalCode($code);
+    ): User {
 
-        if (strlen($password) < 7) {
-            
+        $userSharingusername = $this->userRepository->findOneBy(['login' => $username]);
+
+        if ($userSharingusername !== null) {
+            throw new HttpException(statusCode: Response::HTTP_CONFLICT, message: "this username is already taken");
         }
 
+        $postalCode = $this->postalCodeService->findOrCreatePostalCode($code);
+
+        $user = (new User())
+        ->setLogin($username)
+        ->setPostalCode($postalCode);
+
+        $errors = $this->validator->validate($user);
+
+        if (count($errors) > 0) {
+            throw new HttpException(statusCode: Response::HTTP_INTERNAL_SERVER_ERROR, message: json_encode($errors));
+        }
+
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $password));
+
+        $this->entityManager->persist($user);
+
+        $this->entityManager->flush();
+        
+        return $user;
     }
 }
