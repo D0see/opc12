8d376e39d1ac44ee5d1677e8f91bda1158b75966
D0see <317Y17@gmail.com>
2026-08-28 11:20:44 +0200

    save

--- src/Helper/ErrorHelper.php
+++ src/Helper/ErrorHelper.php
@@ -4,16 +4,19 @@ use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class ErrorHelper {
 
-    public static function spreadContraintViolationsMessages(ConstraintViolationListInterface $errorList) {
+    public static function spreadContraintViolationsMessages(
+        ConstraintViolationListInterface $errorList,
+        string $separator = ', '
+    ) {
         $result = array_reduce(
-            callback: function($acc, $error) {
-                $acc .= $error->getMessage() . ', ';
+            callback: function($acc, $error) use ($separator) {
+                $acc .= $error->getMessage() . $separator;
                 return $acc;
             },
             initial: '',
             array: [...$errorList]
         );
 
-        return substr($result, 0, strlen($result) -2);
+        return substr($result, 0, strlen($result) - strlen($separator));
     }
 }
